67a5f45f7344d242fd04a95bc52bdce8f70946ec
Iago Sousa <iagosousadev@gmail.com>
2020-06-02 14:17:36 -0300

    adicionando base do projeto

---
+++ frontend/src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>YOOOOOOOOOOOOOOo</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
+
+<style lang="stylus">
+  h1
+    color 'red'
+    background 'yellow'
+</style>
